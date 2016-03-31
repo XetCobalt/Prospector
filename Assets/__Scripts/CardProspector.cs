cad3ba97a52913ae70171f2cbccc34383e9857e1
Xavier Martinez <xmtheinventor@gmail.com>
2016-03-31 14:37:47 -0400

    Progress...

---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum CardState{
+	drawpile,
+	tableau,
+	target,
+	discard
+}
+
+public class CardProspector : Card {
+
+	public CardState state = CardState.drawpile;
+
+	public List<CardProspector> hiddenBy = new List<CardProspector>();
+
+	public int layoutID;
+	public SlotDef slotdef;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -9,14 +9,73 @@ public class Prospector : MonoBehaviour {
 	public Deck					deck;
 	public TextAsset			deckXML;
 
+	public Layout layout;
+	public TextAsset layoutXML;
+	public Vector3 layoutCenter;
+	public float xOffset = 3;
+	public float yOffset = -2.5f;
+	public Transform layoutAnchor;
+	
+	public CardProspector target;
+	public List<CardProspector> tableau;
+	public List<CardProspector> discardPile;
+
 	void Awake(){
 		S = this;
 	}
+
+	public List<CardProspector> drawPile;
 
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle (ref deck.cards);
+
+		layout = GetComponent<Layout> ();
+		layout.ReadLayout (layoutXML.text);
+
+		drawPile = ConvertListCardsToListCardProspectors (deck.cards);
+		LayoutGame ();
+	}
+
+	CardProspector Draw(){
+		CardProspector cd = drawPile [0];
+		drawPile.RemoveAt[0];
+		return(cd);
+	}
+
+	void LayoutGame(){
+		if(layoutAnchor == null){
+			GameObject tGO = new GameObject("_LayoutAnchor");
+			layoutAnchor = tGO.transform;
+			layoutAnchor.transform.position = layoutCenter;
+		}
+
+		CardProspector cp;
+
+		foreach(SlotDef tSD in layout.slotDefs){
+			cp = Draw ();
+			cp.faceUP = tSD.faceUp;
+			cp.transform.parent = layoutAnchor;
+
+			cp.transform.localPosition = new Vector3(layout.multiplier.x*tSD.x, layout.multiplier.y*tSD.y, -tSD.layerID);
+
+			cp.layoutID = tSD.id;
+			cp.slotdef = tSD; //SOMETHING MAY GO WRONG HERE BECAUSE BOOK ERROR(?)
+			cp.state = CardState.tableau;
+
+			tableau.Add (cp);
+		}
+	}
+
+	List<CardProspector> ConvertListCardsToListCardProspectors (List<Card> lCD){
+		List<CardProspector> lCP = new List<CardProspector> ();
+		CardProspector tCP;
+		foreach (Card tCD in lCD) {
+			tCP = tCD as CardProspector;
+			lCP.Add (tCP);
+		}
+		return lCP;
 	}
 
 }
